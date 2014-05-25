2c16b5cf427da8b3d65ba136e16faa01de269884
Gabe Wyatt <gwyattkelsey@gmail.com>
2014-05-24 23:10:05 -0700

    Added some basic static pages with titles.

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,40 @@
+require 'spec_helper'
+
+describe "Static Pages" do
+
+  describe "About page" do
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About Us')
+    end
+
+    it "should have the proper title" do
+      visit '/static_pages/about'
+      expect(page).to have_title("VCP | About")
+    end
+  end
+
+  describe "Help page" do
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
+    end
+
+    it "should have the proper title" do
+      visit '/static_pages/help'
+      expect(page).to have_title("VCP | Help")
+    end
+  end
+
+  describe "Home page" do
+    it "should have the content 'VCP'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('VCP')
+    end
+
+    it "should have the proper title" do
+      visit '/static_pages/home'
+      expect(page).to have_title("VCP | Home")
+    end
+  end
+end
