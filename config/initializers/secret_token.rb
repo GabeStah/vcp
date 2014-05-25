72797dfb75144f6d8d5df4900e83c827b87e3d20
Gabe Wyatt <gwyattkelsey@gmail.com>
2014-05-24 20:00:28 -0700

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Vcp::Application.config.secret_key_base = 'ad67fc9df5b1e7619c0124963343ce2c456b68d8de7d3d013d2241014c9a1231221bc42a26a7433e9723d0a3008365898b82324b96230ddfccc8c5aff1aa7321'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Vcp::Application.config.session_store :cookie_store, key: '_vcp_session'
