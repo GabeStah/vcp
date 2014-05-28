9ea329235590014ac5b9890eb8f4b26076a234fa
Gabe Wyatt <gwyattkelsey@gmail.com>
2014-05-28 00:39:43 -0700

    Added secure_password to user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,7 @@ class User < ActiveRecord::Base
   validates :name,
             presence: true,
             length: { maximum: 50 }
+  validates :password,
+            length: { minimum: 6 }
+  has_secure_password
 end
