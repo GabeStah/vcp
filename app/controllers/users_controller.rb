73fe49e05710c8bcaf193b2aedd3703ffa2c8346
Gabe Wyatt <gwyattkelsey@gmail.com>
2014-05-31 10:56:49 -0700

    Added Users index page.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+
   def create
     @user = User.new(user_params)
     if @user.save
@@ -11,7 +14,10 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
+  end
+
+  def index
+    @users = User.all
   end
 
   def new
@@ -31,9 +37,20 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
+  private
 
-  def user_params
-    params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
-  end
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+
+    def signed_in_user
+      store_location
+      redirect_to signin_url, notice: "Please sign in." unless signed_in?
+    end
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
