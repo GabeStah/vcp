eb81bf3fed97e4828ccff3b5c314bbe3022f8cea
Gabe Wyatt <gwyattkelsey@gmail.com>
2014-05-31 06:54:00 -0700

    Finalized User authentation for signin/signup/signout.

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,12 @@ Vcp::Application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy]
 
   root 'static_pages#home'
-  get '/about' => 'static_pages#about'
-  get '/contact' => 'static_pages#contact'
-  get '/help' => 'static_pages#help'
-  get '/signin' => 'sessions#new'
-  get '/signout' => 'sessions#destroy'
-  get '/signup' => 'users#new'
+  match '/about',   to: 'static_pages#about',   via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
+  match '/help',    to: 'static_pages#help',    via: 'get'
+  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
+  match '/signup',  to: 'users#new',            via: 'get'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
