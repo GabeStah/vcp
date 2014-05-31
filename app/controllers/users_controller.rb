7937b116f163f4cc7b4be8ba048b8a73eed67c84
Gabe Wyatt <gwyattkelsey@gmail.com>
2014-05-31 10:37:51 -0700

    Added User edit methods.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,12 +10,26 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
   def new
     @user = User.new
   end
 
   def show
     @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
   end
 
   def user_params
