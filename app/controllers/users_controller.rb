411a3cbe9250a76b58a1a75649e995670ef449ea
Gabe Wyatt <gwyattkelsey@gmail.com>
2014-05-31 11:22:30 -0700

    Completed User index page, updating, and destroying methods for Admin users.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :signed_in_user, only: [:edit, :update]
   before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
 
   def create
     @user = User.new(user_params)
@@ -13,11 +14,17 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted."
+    redirect_to users_url
+  end
+
   def edit
   end
 
   def index
-    @users = User.all
+    @users = User.paginate(page: params[:page])
   end
 
   def new
@@ -38,6 +45,10 @@ class UsersController < ApplicationController
     end
   end
   private
+
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
 
     def correct_user
       @user = User.find(params[:id])
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,14 @@
 FactoryGirl.define do
-  factory :user do
-    name     "Gabe Wyatt"
-    email    "gwyattkelsey@gmail.com"
-    password "hobbes"
-    password_confirmation "hobbes"
+  FactoryGirl.define do
+    factory :user do
+      sequence(:name)  { |n| "Person #{n}" }
+      sequence(:email) { |n| "person_#{n}@example.com"}
+      password "foobar"
+      password_confirmation "foobar"
+
+      factory :admin do
+        admin true
+      end
+    end
   end
 end
