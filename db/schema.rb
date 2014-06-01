2199aebd176198418e25a5f94e3925af9391f5d7
Gabe Wyatt <gwyattkelsey@gmail.com>
2014-06-01 05:38:46 -0700

    Added initial Race model with basic testing/validation.

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140531180642) do
+ActiveRecord::Schema.define(version: 20140601121048) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "races", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "name"
