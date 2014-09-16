8c0f25bcf0d0ea6b8d9c1e3e15f38a90d8c2aa90
GabeStah <gwyattkelsey@gmail.com>
2014-09-16 01:36:18 -0700

    Removed *.js precompile for production deployment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,4 @@ Vcp::Application.configure do
 
   # Host
   config.action_mailer.default_url_options = { host: 'vcp.gabestah.com' }
-
-  # Precompile for production
-  config.assets.precompile = ['*.js', '*.css', '*.css.erb']
 end
