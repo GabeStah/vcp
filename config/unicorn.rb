95d9e8843874b916df11aa4307d81a704c8f230d
GabeStah <gwyattkelsey@gmail.com>
2014-10-31 09:38:36 -0700

    Added appropriate activerecord::base checks to unicorn.rb config.

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -8,7 +8,6 @@ shared_path = "/var/www/vcp/shared"
 
 # Set unicorn options
 worker_processes 1
-preload_app false
 timeout 180
 #listen "127.0.0.1:9000"
 listen "#{shared_path}/tmp/sockets/unicorn.sock"
@@ -29,8 +28,17 @@ stdout_path "#{shared_path}/log/unicorn.log"
 # Set master PID location
 pid "#{shared_path}/tmp/pids/unicorn.pid"
 
+# combine Ruby 2.0.0dev or REE with "preload_app true" for memory savings
+# http://rubyenterpriseedition.com/faq.html#adapt_apps_for_cow
+preload_app true
+GC.respond_to?(:copy_on_write_friendly=) and
+  GC.copy_on_write_friendly = true
+
 before_fork do |server, worker|
-  ActiveRecord::Base.connection.disconnect
+  # the following is highly recomended for Rails + "preload_app true"
+  # as there's no need for the master process to hold a connection
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.connection.disconnect!
 
   old_pid = "#{server.config[:pid]}.oldbin"
   if File.exists?(old_pid) && server.pid != old_pid
@@ -43,5 +51,7 @@ before_fork do |server, worker|
 end
 
 after_fork do |server, worker|
-  ActiveRecord::Base.establish_connection
+  # the following is *required* for Rails + "preload_app true",
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.establish_connection
 end
