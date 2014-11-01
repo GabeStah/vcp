3008e9304ad78d37a0db0782772b8686121dbbf8
GabeStah <gwyattkelsey@gmail.com>
2014-11-01 10:16:15 -0700

    Added AjaxDatatablesRails::Base.search_condition override to support proper type casting for Mysql2 adapter.

---
+++ config/initializers/ajax_data_tables_base.rb
@@ -0,0 +1,12 @@
+module AjaxDatatablesRails
+  class Base
+    extend Forwardable
+
+    def search_condition(column, value)
+      model, column = column.split('.')
+      model = model.singularize.titleize.gsub( / /, '' ).constantize
+      casted_column = ::Arel::Nodes::NamedFunction.new('CAST', [model.arel_table[column.to_sym].as(ActiveRecord::Base::connection.is_a?(ActiveRecord::ConnectionAdapters::Mysql2Adapter) ? 'CHAR' : 'VARCHAR')])
+      casted_column.matches("%#{value}%")
+    end
+  end
+end
